9e212aa77659174a0e4d641133951715018a2249
thinhlang <quocthinh861@gmail.com>
2023-07-20 17:50:44 +0700

    update code

---
+++ Sportyfind-CMS/Sportyfind-CMS/Controllers/FieldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sportyfind_CMS.Controllers
+{
+    public class FieldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
